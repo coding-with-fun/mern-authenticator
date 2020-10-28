e8a1e34c18e6345784f79a2ae009b2989564a55e
Harsh <harshpatel.softvan@gmail.com>
2020-10-28 08:18:57 +0530

    make it better

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const path = require("path");
 
 require("colors");
 require("dotenv").config();
@@ -20,3 +21,13 @@ connectDB();
 
 // * Defining routes
 app.use("/user", require("./routes/user.api"));
+
+// * Server static files
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("../client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "../client", "build", "index.html")
+        );
+    });
+}
