333e48767e782b9f0f0be9d2c03c055a2591f7df
Harsh <harshpatel.softvan@gmail.com>
2020-10-28 10:04:29 +0530

    make it better

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use("/user", require("./routes/user.api"));
 
 // * Server static files
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../client/build"));
+    app.use(express.static("client/build"));
     app.get("*", (req, res) => {
         res.sendFile(
             path.resolve(__dirname, "../client", "build", "index.html")
